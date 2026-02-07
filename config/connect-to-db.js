e18cfb60649be4452e5cd0679b79a19b2c340f52
Youssef Saeed (Jo+) <saeedyoussef219@gmail.com>
2026-02-07 17:28:09 +0200

    Update connect-to-db.js

--- config/connect-to-db.js
+++ config/connect-to-db.js
@@ -1,10 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-module.exports = async ()=>{
+module.exports = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("connected To DB ^_^");
-  }catch (err){
-    console.log("connection Failed To DB !" , err);
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 5000,
+    });
+    console.log("Connected To DB ^_^");
+  } catch (err) {
+    console.log("Connection Failed To DB!", err.message);
   }
-}
+};
